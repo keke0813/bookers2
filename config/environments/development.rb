283ed61adeb5a70ade4ad1e28a7cf8a5bccdecfe
keke0813 <149664866+keke0813@users.noreply.github.com>
2023-12-13 13:09:14 +0000

    課題チェックシート　データベース上に必要なテーブルを作成まで完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c2fdaa544042426b9e2a6a03766f6ad4.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
 end
