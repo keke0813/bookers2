0fe0b4302fd3f3d986ac8537597c78bb33aad115
keke0813 <149664866+keke0813@users.noreply.github.com>
2023-12-14 06:38:26 +0000

    課題チェックシート　aboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   resources :books, only: [:new, :index, :show]
   resources :users, only: [:show, :edit]
+  get '/homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
