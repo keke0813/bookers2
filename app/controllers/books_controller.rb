95396948d0bf22c9a6096129cd3b54d7db30394a
keke0813 <149664866+keke0813@users.noreply.github.com>
2023-12-16 12:52:50 +0000

    bookのindexページの左側の作成(User info)(New book)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,28 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+  
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
   def show
   end
-  
+
   def index
-    @users = User.all
   end
 
   def edit
+    @user = User.find(params[:id])
+    render :edit
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
