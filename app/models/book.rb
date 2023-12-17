04b613a702b215139f6545ab03361fc3fbe186fd
keke0813 <149664866+keke0813@users.noreply.github.com>
2023-12-17 02:07:22 +0000

    bookのdestroy機能の作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
-  
+
   belongs_to :user
-  
+
   has_one_attached :profile_image
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -11,5 +11,5 @@ class Book < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
 end
